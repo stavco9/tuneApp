4139d26e36e6342c7b26a1319f93d5b4711c13a7
EdenT <edmx0.et@gmail.com>
2019-02-16 19:41:19 -0800

    chore(app): added the first working app

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'NOT NOIIIIICE',
+  android:
+    'NOIIIIICE',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to TuneApp</Text>
+        <Text style={styles.instructions}>YO MAMA</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
